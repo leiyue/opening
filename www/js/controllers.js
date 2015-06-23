a5e6a4d38ea2c66ef55cc1414dc8c4958a9e2237
leiyue <mr.leiyue@gmail.com>
2015-06-24 00:04:29 +0800

    first comment

---
+++ www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('opening.controllers', [])
+
+    .controller('HomeCtrl', function ($scope, $http, $ionicPopup) {
+        var vm = $scope.vm = {};
+
+        vm.user = {};
+        vm.submit = function (user) {
+            console.log(user);
+            $ionicPopup.alert({
+                title: 'POST数据测试',
+                template: JSON.stringify(user),
+                okText: '确定',
+                okType: 'button-custom'
+            }).then(function(res){
+                $http({
+                    //url: '/data/success.json',
+                    url: '/data/failure.json',
+                    method: "GET"
+                    //method: "POST",
+                    //data: user
+                }).success(function (data) {
+                    $ionicPopup.alert({
+                        title: data.status ? '操作成功':'操作失败',
+                        template: JSON.stringify(data),
+                        okText: '确定',
+                        okType: 'button-custom'
+                    });
+                });
+            });
+        }
+    });
