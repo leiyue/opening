ce6a5039e02d6814d35748eca314d396730355c8
leiyue <mr.leiyue@gmail.com>
2015-06-26 10:55:09 +0800

    对话框按钮调整

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,11 +10,11 @@ angular.module('opening.controllers', [])
                 title: 'POST数据测试',
                 template: JSON.stringify(user),
                 okText: '确定',
-                okType: 'button-custom'
+                okType: 'button-energized'
             }).then(function(res){
                 $http({
-                    //url: '/data/success.json',
-                    url: '/data/failure.json',
+                    url: '/data/success.json',
+                    //url: '/data/failure.json',
                     method: "GET"
                     //method: "POST",
                     //data: user
@@ -23,7 +23,7 @@ angular.module('opening.controllers', [])
                         title: data.status ? '操作成功':'操作失败',
                         template: JSON.stringify(data),
                         okText: '确定',
-                        okType: 'button-custom'
+                        okType: 'button-energized'
                     });
                 });
             });
